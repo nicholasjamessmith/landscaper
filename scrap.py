cd51a97656820e91163dfd41281ec262e08265b2
nicholasjamessmith <njsmith6@gmail.com>
2023-09-05 11:44:51 -0400

    user can use scissors to cut grass (w/ bugs)

--- scrap.py
+++
@@ -1,8 +0,0 @@
-    player_choice = int(input("""
-                            You can...
-                            [2] Quit Lanscaper
-                            [4] Make $5 using your rusty scissors to cut the lawn?
-                          """))
-    if (player_choice == 4):
-      game_data["money"] += 5
-      print(f"You now have a total of {game_data['money']} money")
